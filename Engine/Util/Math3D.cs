a5e27b94f4aafa077ce91f06f19ae7e9abafa3ef
jonas <jonas.gilg@stud.hs-hannover.de>
2017-10-19 00:41:50 +0200

    Added Util function for VectorRotation

---
+++ Engine/Util/Math3D.cs
@@ -0,0 +1,12 @@
+﻿using OpenTK;
+
+namespace Engine.Util {
+	public static class Math3D {
+		public static Vector3 Rotate(Vector3 v, Quaternion r) {
+			var u = new Vector3(r.Xyz);
+			var s = r.W;
+
+			return 2.0f * Vector3.Dot(v, u) * u + (s * s - Vector3.Dot(u, u)) * v + 2.0f * s * Vector3.Cross(u, v);
+		}
+	}
+}
