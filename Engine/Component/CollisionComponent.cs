a6b59bcee56270808098ee9188bc7a23f420a18c
Michael <michael.weissmann@stud.hs-hannover.de>
2017-10-29 21:27:12 +0100

    endlich bissl gearbeitet^^

---
+++ Engine/Component/CollisionComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Component {
+	class CollisionComponent : Component {
+		public CollisionComponent(GameObject gameObject) : base(gameObject) {
+			Console.WriteLine("Hello Micha");
+		}
+	}
+}
