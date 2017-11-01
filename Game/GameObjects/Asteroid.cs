70efab3a63f4ae53c10ca9a81f8625ddf1d3d220
torben <torben.lange89@gmail.com>
2017-11-01 22:53:02 +0100

    Asteroid object added

---
+++ Game/GameObjects/Asteroid.cs
@@ -0,0 +1,26 @@
+﻿using Engine.Component;
+using Engine;
+using Engine.Material;
+using Engine.Model;
+using Engine.Util;
+using OpenTK;
+
+namespace Game.GameObjects
+{
+    public class Asteroid: GameObject
+    {
+        public readonly MoveComponent MoveComponent;
+        public readonly Model3D Model;
+
+        public Asteroid(Model3D model3D, Vector3 scaleVector3)
+        {
+            Model = model3D;
+            MoveComponent = new MoveComponent(this);
+            Transform
+
+        }
+        
+        
+        
+    }
+}
