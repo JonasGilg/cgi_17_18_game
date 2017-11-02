ad9632c24496aef340ab578ea109fbe8660bec3e
jonas <jonas.gilg@stud.hs-hannover.de>
2017-11-02 18:03:22 +0100

    made diffuse and specular part 0 if the surface isn't facing the light source

--- Engine/Material/AmbientDiffuseSpecular/AmbientDiffuseSpecular_FS.glsl
+++ Engine/Material/AmbientDiffuseSpecular/AmbientDiffuseSpecular_FS.glsl
@@ -1,6 +1,8 @@
 #version 330
 precision highp float;
 
+#define M_PI 3.1415926535897932384626433832795
+
 uniform sampler2D sampler; 
 
 // "model_matrix" Matrix
@@ -30,17 +32,26 @@ void main() {
 
 	// calculate normal
     vec3 normal = normalize(normalMatrix * fragNormal);
+    
+    //angle between surface normal and light direction
+    float angle = acos(dot(light_direction, normal));
 
-	// calclulate view direction
-	vec4 v = normalize(camera_position - fragPosition);
-
-	// calculate halfway vector
-	vec3 h = normalize(light_direction + vec3(v));
-	float ndoth = dot(normal, h);
-	float specularIntensity = pow(ndoth, specular_shininess);
-
-	// calculate brightness for diffuse light
-	float brightness = dot(normal, light_direction);
+    float specularIntensity = 0;
+    float brightness = 0;
+    
+    //if the surface isn't facing the light source the diffuse and specular part stay zero.
+    if(angle < M_PI / 2) {
+        // calclulate view direction
+        vec4 v = normalize(camera_position - fragPosition);
+    
+        // calculate halfway vector
+        vec3 h = normalize(light_direction + vec3(v));
+        float ndoth = dot(normal, h);
+        specularIntensity = pow(ndoth, specular_shininess);
+    
+        // calculate brightness for diffuse light
+        brightness = dot(normal, light_direction);
+	}
 	
 	// surfaceColor is color from the texture
 	vec4 surfaceColor = texture2D(sampler, fragTexcoord);
