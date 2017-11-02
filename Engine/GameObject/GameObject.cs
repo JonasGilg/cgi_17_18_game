79e10cdaaf8bff019a854fc3076eb198267c23c3
Michael <michael.weissmann@stud.hs-hannover.de>
2017-11-02 19:24:08 +0100

    added collision classes

--- Engine/GameObject/GameObject.cs
+++ Engine/GameObject/GameObject.cs
@@ -1,16 +1,32 @@
 ﻿using Engine;
 using OpenTK.Input;
+using System;
 
 namespace Engine {
 	public class GameObject {
 		public readonly TransformComponent TransformComponent;
+		public CollisionComponent collisionComponent;
+		public string name;
+		public string tag;
 
 		public GameObject() {
 			TransformComponent = TransformComponent.Identity();
 		}
-		
+
+		public virtual void EarlyUpdate() {
+
+		}
+
+		public virtual void LateUpdate() {
+
+		}
+
 		public virtual void Update(double deltaTime, KeyboardDevice input) {
 			TransformComponent.Update(deltaTime, input);
+		}
+
+		public virtual void OnCollisionEnter(Collision collision) {
+			Console.WriteLine(collision.gameObjects[0].name + " and " + collision.gameObjects[1].name + " collided!");
 		}
 	}
 }
