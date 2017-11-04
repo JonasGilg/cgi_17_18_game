a17376486d51af4567da1cb4ffa55139ee3b82e6
jonas <jonas.gilg@stud.hs-hannover.de>
2017-11-04 01:04:46 +0100

    camera has now a max y turn value

--- Engine/Camera/ThirdPersonCameraComponent.cs
+++ Engine/Camera/ThirdPersonCameraComponent.cs
@@ -1,4 +1,5 @@
-﻿using Engine.Util;
+﻿using System;
+using Engine.Util;
 using OpenTK;
 using OpenTK.Input;
 using Mouse = Engine.Input.Mouse;
@@ -7,30 +8,37 @@ namespace Engine {
 	public class ThirdPersonCameraComponent : CameraComponent {
 		public Vector3 Offset;
 		private Vector2 _rotation;
-		
+
 		public ThirdPersonCameraComponent(Vector3 offset, GameObject gameObject) : base(gameObject) {
 			Offset = offset;
 			_rotation = Vector2.Zero;
 		}
-		
+
 		public override void Update() {
 			base.Update();
 
 			if (Mouse.Down(MouseButton.Right)) {
-				_rotation.Y += -Mouse.CursorDelta.X * Time.DeltaTime * 0.1f;
-				_rotation.X += -Mouse.CursorDelta.Y * Time.DeltaTime * 0.1f;
+				var factor = Time.DeltaTime * 0.1f;
+				
+				_rotation.X += -Mouse.CursorDelta.X * factor;
+
+				const float maxRotation = (float) (Math.PI / 2 - 0.2f);
+				const float minRotation = -maxRotation;
+				var deltaY = -Mouse.CursorDelta.Y * factor;
+				_rotation.Y = Math1D.Clamp(_rotation.Y + deltaY, minRotation, maxRotation);
 			}
 			else {
 				_rotation = Vector2.Lerp(_rotation, Vector2.Zero, Time.DeltaTime * 5);
 			}
-			
-			var rotation = Quaternion.FromEulerAngles(_rotation.X, _rotation.Y, 0f);
+
+			var rotation = Quaternion.FromEulerAngles(_rotation.Y, _rotation.X, 0f);
 			var eyePosition = Math3D.Rotate(Offset, rotation);
 			Math3D.Rotate(ref eyePosition, GameObject.TransformComponent.Orientation);
 
 			eyePosition += GameObject.TransformComponent.Position;
 
-			SetLookAt(eyePosition, GameObject.TransformComponent.Position, GameObject.TransformComponent.Orientation * Vector3.UnitY);
+			SetLookAt(eyePosition, GameObject.TransformComponent.Position,
+				GameObject.TransformComponent.Orientation * Vector3.UnitY);
 		}
 	}
 }
