9ecf1d1398727f2e392013f4532c12354951de69
Michael <michael.weissmann@stud.hs-hannover.de>
2017-11-20 01:26:52 +0100

    WIP Sun lava flow shader

--- Engine/Render/Material/MaterialManager.cs
+++ Engine/Render/Material/MaterialManager.cs
@@ -7,7 +7,8 @@ namespace Engine.Material {
 		SIMPLE_REFLECTION,
 		AMBIENT_DIFFUSE_SPECULAR,
 		NORMAL_MAPPING,
-		BLACK_HOLE
+		BLACK_HOLE,
+		SUN_LAVAFLOW
 	}
 
 	public static class MaterialManager {
@@ -19,7 +20,8 @@ namespace Engine.Material {
 				[Material.SIMPLE_REFLECTION] = new SimpleReflectionMaterial(),
 				[Material.AMBIENT_DIFFUSE_SPECULAR] = new AmbientDiffuseSpecularMaterial(),
 				[Material.NORMAL_MAPPING] = new NormalMappingMaterial(),
-				[Material.BLACK_HOLE] = new BlackHoleMaterial()
+				[Material.BLACK_HOLE] = new BlackHoleMaterial(),
+				[Material.SUN_LAVAFLOW] = new SunLavaflowMaterial()
 			};
 		}
 
