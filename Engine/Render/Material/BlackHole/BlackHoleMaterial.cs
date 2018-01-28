7409908a2eb7f48c39e2d57235620bdd20ec8717
Michael <michael.weissmann@stud.hs-hannover.de>
2018-01-28 20:00:51 +0100

    Black hole shader is done!

--- Engine/Render/Material/BlackHole/BlackHoleMaterial.cs
+++ Engine/Render/Material/BlackHole/BlackHoleMaterial.cs
@@ -1,22 +1,15 @@
 ﻿using System;
 using Engine.Model;
 using Engine.Render;
+using Engine.Util;
 using OpenTK;
 using OpenTK.Graphics.OpenGL;
 
 namespace Engine.Material {
 	public class BlackHoleMaterial : BaseMaterial {
 		public int modelviewProjectionMatrixLocation;
-
-		public int materialPositionLocation;
-		public int materialRatioLocation;
-		public int materialRadiusLocation;
-		public int materialDistanceLocation;
-
-		public float ratio;
-
-		//radius of the effect of the black hole
-		public float radius = 50.0f;
+		public int modelMatrixLocation;
+		public int cameraPositionLocation;
 
 		public BlackHoleMaterial() {
 			CreateShaderProgram("Render/Material/BlackHole/BlackHole_VS.glsl",
@@ -26,26 +19,22 @@ namespace Engine.Material {
 			// GL.BindAttribLocation, gibt an welcher Index in unserer Datenstruktur welchem "in" Parameter auf unserem Shader zugeordnet wird
 			// folgende Befehle müssen aufgerufen werden...
 			GL.BindAttribLocation(Program, 0, "in_position");
-			GL.BindAttribLocation(Program, 1, "in_uv");
+			GL.BindAttribLocation(Program, 1, "in_normal");
+			GL.BindAttribLocation(Program, 2, "in_uv");
 
 			// ...bevor das Shader-Programm "gelinkt" wird.
 			GL.LinkProgram(Program);
 
 			// Die Stelle an der im Shader der per "uniform" der Input-Paremeter "modelview_projection_matrix" definiert wird, wird ermittelt.
 			modelviewProjectionMatrixLocation = GL.GetUniformLocation(Program, "modelview_projection_matrix");
-
-			materialPositionLocation = GL.GetUniformLocation(Program, "position");
-			materialRatioLocation = GL.GetUniformLocation(Program, "ratio");
-			materialRadiusLocation = GL.GetUniformLocation(Program, "radius");
-			materialDistanceLocation = GL.GetUniformLocation(Program, "distance");
+			modelMatrixLocation = GL.GetUniformLocation(Program, "model_matrix");
+			cameraPositionLocation = GL.GetUniformLocation(Program, "camera_pos");
+		
 		}
 
 		protected override void PreDraw() {
 			GL.UseProgram(Program);
 
-			GL.Uniform2(materialPositionLocation, new Vector2(0.5f, 0.5f));
-			GL.Uniform1(materialRatioLocation, 0.5625f);
-			GL.Uniform1(materialRadiusLocation, 200.0f);
 		}
 
 		protected override void PostDraw() {
@@ -58,7 +47,12 @@ namespace Engine.Material {
 
 			var modelviewProjection = (model.Transformation * DisplayCamera.Transformation * DisplayCamera.PerspectiveProjection).ToFloat();
 			GL.UniformMatrix4(modelviewProjectionMatrixLocation, false, ref modelviewProjection);
-			GL.Uniform1(materialDistanceLocation, Math3D.Distance(model.Transformation.ExtractTranslation().ToFloat(), DisplayCamera.Position.ToFloat()));
+			IO.PrintAsync("mvp: " + modelviewProjection);
+			var modelmatrix = model.Transformation.ToFloat();
+			GL.UniformMatrix4(modelMatrixLocation, false, ref modelmatrix);
+			IO.PrintAsync("model: " + modelmatrix);
+			GL.Uniform3(cameraPositionLocation, DisplayCamera.Position.ToFloat());
+			IO.PrintAsync("camera: " + DisplayCamera.Position.ToFloat());
 
 			GL.DrawElements(PrimitiveType.Triangles, model.Indices.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
 		}
